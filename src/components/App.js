2a30ac0c77013cb5d4de0d839b72551c346381df
raaz-prince <krprince006@gmail.com>
2024-07-30 02:54:57 +0530

    packingApp

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import  Stats  from "./Stats";
+import PackingList from "./PackingList"
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+    console.log(items);
+  }
+
+  function handleClearList() {
+    const confimed = window.confirm("Are you sure you want to delete all items");
+    if(confimed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItems}
+        onToggleItems={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
